21310244046cb355acc03461710fd72f7137ea38
CorgiTaco <>
2020-06-17 19:13:59 -0700

    EEEEE

--- src/main/java/isuckatusernames/mw/MumblingWoods.java
+++ src/main/java/isuckatusernames/mw/MumblingWoods.java
@@ -7,14 +7,10 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tag.BlockTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
-
-import java.util.OptionalLong;
 
 public class MumblingWoods implements ModInitializer {
 	public static String MODID = "mumbling_woods";
@@ -25,8 +21,8 @@ public class MumblingWoods implements ModInitializer {
 	public static ItemGroup SVW_GROUP = FabricItemGroupBuilder.build(new Identifier("mumbling_woods", "mumbling_woods"), () -> new ItemStack(PORTAL_BLOCK));
 	public static final RegistryKey<World> MUMBLING_WOODS_WORLD = RegistryKey.of(Registry.DIMENSION, new Identifier("mumbling_woods", "mumbling_woods"));
 
-	public static final DimensionType MUMBLING_WOODS = new MumblingWoodsDimensionType(OptionalLong.of(6000L), false, false, false, false, false, true, false, false, false, 256, BlockTags.INFINIBURN_END.getId(), 0.0F);
-	public static final RegistryKey<DimensionType> MUMBLING_WOODS_REGISTRY_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY, new Identifier("mumbling_woods_type"));;
+//	public static final DimensionType MUMBLING_WOODS = new MumblingWoodsDimensionType(OptionalLong.of(6000L), false, false, false, false, false, true, false, false, false, 256, BlockTags.INFINIBURN_END.getId(), 0.0F);
+//	public static final RegistryKey<DimensionType> MUMBLING_WOODS_REGISTRY_KEY = RegistryKey.of(Registry.DIMENSION_TYPE_KEY, new Identifier("mumbling_woods_type"));;
 
 
 	@Override
--- src/main/java/isuckatusernames/mw/mixin/MixinDimensionType.java
+++ src/main/java/isuckatusernames/mw/mixin/MixinDimensionType.java
@@ -1,18 +1,10 @@
 package isuckatusernames.mw.mixin;
 
-import isuckatusernames.mw.MumblingWoods;
-import net.minecraft.util.registry.RegistryTracker;
-import net.minecraft.world.dimension.DimensionType;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-@Mixin(DimensionType.class)
-public class MixinDimensionType {
-
-    @Inject(at = @At("HEAD"), method = "addRegistryDefaults", cancellable = true)
-    private static void addMumblingWoodsRegistryDefault(RegistryTracker.Modifiable registryTracker, CallbackInfoReturnable<RegistryTracker.Modifiable> cir) {
-        registryTracker.addDimensionType(MumblingWoods.MUMBLING_WOODS_REGISTRY_KEY, MumblingWoods.MUMBLING_WOODS);
-    }
-}
+//@Mixin(DimensionType.class)
+//public class MixinDimensionType {
+//
+//    @Inject(at = @At("HEAD"), method = "addRegistryDefaults", cancellable = true)
+//    private static void addMumblingWoodsRegistryDefault(RegistryTracker.Modifiable registryTracker, CallbackInfoReturnable<RegistryTracker.Modifiable> cir) {
+//        registryTracker.addDimensionType(MumblingWoods.MUMBLING_WOODS_REGISTRY_KEY, MumblingWoods.MUMBLING_WOODS);
+//    }
+//}
